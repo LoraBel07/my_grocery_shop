54f461790b3f9c2e5f17ad977322edd2a9719564
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2024-04-06 22:24:59 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import image from './apple.png';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src={ image } width="300px" alt="apple"/>
+      
+    </div>
+  );
+}
+
+export default App;
