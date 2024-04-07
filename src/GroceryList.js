ad0f67a6257d62856aec73a6f8994ca6b88467ff
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2024-04-07 14:52:05 -0400

    commit

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -3,20 +3,46 @@ import { Component } from 'react';
 
 export class GroceryList extends Component {
 	state = {
+		userInput: '',
+		groseryList: []
+
 
 	}
 
-	onChangeEvent(event) {
-		console.log(event.target.value);
+	onChangeEvent(e) {
+		this.setState({userInput: e})
+		
+	}
+	addItem(input) {
+		let listArray = this.state.groseryList;
+		listArray.push(input);
+		this.setState({ GroceryList: listArray, userInput: '' })
+		
 	}
 
 	render() {
 		return(
 			<div>
-				<input type='text' 
-				placeholder='What do you want to buy?'
-				onChange={this.onChangeEvent} />
+
+				<div>
+					<input type='text'
+					placeholder='What do you want to buy?'
+					onChange={(e) => {this.onChangeEvent(e.target.value)}}
+					value={this.state.userInput} />
+				</div>
+				<div>
+					<button onClick={() => this.addItem(this.state.userInput)}>Add</button>
+				</div>
+				<ul>
+					{this.state.groseryList.map((item, index) => (
+						<li key={index}>{item}</li>
+					))}
+					
+				</ul>
+
 			</div>
+
+
 		)
 
 	}
